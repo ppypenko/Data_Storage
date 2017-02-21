26f41389d97a91d5786eb92cff4ec49374a0a880
ppypenko <ppypenko@student.neumont.edu>
2017-02-21 07:22:07 -0700

    Created project

---
+++ data_storage/app/src/main/java/team/afalse/data_storage/MainActivity.java
@@ -0,0 +1,13 @@
+package team.afalse.data_storage;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
