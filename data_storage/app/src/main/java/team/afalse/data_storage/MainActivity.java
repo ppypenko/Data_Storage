8f8533a433d8ffa401cc143d2c2cb52b7b842025
Kyle Billiot <kylejbilliot@gmail.com>
2017-02-22 08:14:45 -0700

    Added build.gradle support library, and added ListView to main layout, with filling.

--- data_storage/app/src/main/java/team/afalse/data_storage/MainActivity.java
+++ data_storage/app/src/main/java/team/afalse/data_storage/MainActivity.java
@@ -4,16 +4,31 @@ import android.media.AudioManager;
 import android.media.ToneGenerator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DBhandler db;
+    private ListView taskList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        db = new DBhandler(this);
+        taskList = (ListView)findViewById(R.id.allTasks);
+        fillTask();
     }
 
-    private void Alarm(){
+    private void fillTask() {
+        ArrayAdapter<Task> taskArrayAdapter = new ArrayAdapter<Task>(
+                this, android.R.layout.simple_spinner_dropdown_item, db.getAllTasks()
+        );
+        taskList.setAdapter(taskArrayAdapter);
+    }
+
+    private void alarm(){
         ToneGenerator tone = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
         tone.startTone(ToneGenerator.TONE_CDMA_ABBR_ALERT, 200);
     }
